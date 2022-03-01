e76d20de82fe43eb61e37cac4b0fbc44751623b8
Andy Schönfeld <schonfeldandy@gmail.com>
2022-03-01 16:35:48 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Stardew Valley JSON Exporter
+
+Export everything about the game as JSON!
+
---
+++ src/ModEntry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using StardewModdingAPI;
+
+namespace JsonExporter
+{
+    public class ModEntry : Mod 
+    {
+        public override void Entry(IModHelper helper)
+        {
+            helper.ConsoleCommands.Add("export", "export all data", (cmd, args) =>
+            {
+                this.Monitor.Log("hello world!", LogLevel.Info);
+            });
+        }
+    }
+}
